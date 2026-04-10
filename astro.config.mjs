44dc297b054ab40d4250da608d6a6369c0dd3776
Simon Martens <simon.martens@mailbox.org>
2026-04-10 17:56:07 +0200

    Test  WF

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://simon-martens.github.io',
+  base: '/j-p-hebel',
   vite: {
     plugins: [tailwindcss()]
   }
