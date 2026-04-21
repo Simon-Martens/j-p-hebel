4d61f4616ea260c5a3407bea518c5cf79a26de7a
Simon Martens <simon.martens@mailbox.org>
2026-04-21 14:43:54 +0200

    FIX: hebel-archiv.de

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://j-p-hebel.de',
+  site: 'https://hebel-archiv.de',
   base: '/',
   vite: {
     plugins: [tailwindcss()]
